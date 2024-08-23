b50e0b6cca1f0711f649623cef2f7b39a18c043e
Davi Bonfim <124697207+DaviBMiranda@users.noreply.github.com>
2024-08-23 07:40:44 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import BuscarCidade from "./components/BuscarCidade/BuscarCidade"
+
+function App() {
+
+  return (
+    <div className="div-main">
+      <h1>Previsão do Tempo</h1>
+      <BuscarCidade />
+    </div>
+  )
+}
+
+export default App
